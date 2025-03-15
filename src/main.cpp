3795100af08229023a70d5a2ea20e310689a803d
Kalaidar <net.pyat@yandex.ru>
2025-03-15 15:31:31 +0300

    +send data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <PS4Controller.h>
 #include "BluetoothSerial.h"
 
+int r = 255;
+int g = 0;
+int b = 0;
+
+// Calculates the next value in a rainbow sequence
+void nextRainbowColor() {
+  if (r > 0 && b == 0) {
+    r--;
+    g++;
+  }
+  if (g > 0 && r == 0) {
+    g--;
+    b++;
+  }
+  if (b > 0 && g == 0) {
+    r++;
+    b--;
+  }
+}
+
+
 BluetoothSerial BT;
 
 void setup() {
@@ -68,9 +89,24 @@ void loop() {
     Serial.printf("Battery Level : %d\n", PS4.Battery());
 
     Serial.println();
+    PS4.setLed(r, g, b);
+    nextRainbowColor();
+
+    // Sets how fast the controller's front light flashes
+    // Params: How long the light is on in ms, how long the light is off in ms
+    // Range: 0->2550 ms, Set to 0, 0 for the light to remain on
+    PS4.setFlashRate(PS4.LStickY() * 10, PS4.RStickY() * 10);
+
+    // Sets the rumble of the controllers
+    // Params: Weak rumble intensity, Strong rumble intensity
+    // Range: 0->255
+    PS4.setRumble(PS4.L2Value(), PS4.R2Value());
+
+    // Sends data set in the above three instructions to the controller
+    PS4.sendToController();
     // This delay is to make the output more human readable
     // Remove it when you're not trying to see the output
-    delay(1000);
+    delay(100);
   }
 }
 
